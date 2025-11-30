af9b371f09bbe816e10f3d84d6396d5841f51a93
Navaneeth_Joy <navaneethjoy04@gmail.com>
2025-11-30 12:32:37 +0530

    Update noguess.py

--- noguess.py
+++ noguess.py
@@ -1,13 +1,20 @@
 import random
+count=0
 randvar = random.randint(1,100)
-while(True):
-    user_Num=int(input("enter the number"))
-    if (user_Num>randvar):
-                    print("a little less")
-    elif(user_Num<randvar):
-                    print("a little higher")
-    else:
-        print("you've guessed it correctly")
-        break
-        
-          
+print("____WELCOME TO NUMBER GUESSING GAME___")
+print("__YOUVE GOT 5 CHANCES__")
+while(count<=5):
+        count+=1
+        user_Num=int(input("ENTER THE NUMBER\n"))
+
+        if(user_Num==randvar):
+            print("CORRCT!\n")
+            break
+        elif(user_Num>randvar):
+                    print("TOO HIGH!\n")
+        else:
+                    print("TOO LOW!\n")
+else:
+        print("OUT OF ATTEMTS\n" f"THE CURRECT NUMBER IS {randvar}")
+
+  
